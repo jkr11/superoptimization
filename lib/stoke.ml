596f5e679a79459ba63042916da6b515c79a79e2
jkr11 <113939131+jkr11@users.noreply.github.com>
2025-11-10 13:12:10 +0100

    generalize to n registers

--- lib/stoke.ml
+++ lib/stoke.ml
@@ -25,7 +25,7 @@ let string_of_program prog =
   String.concat ~sep:"; " (List.map prog ~f:string_of_instr)
 
 
-let eval_program prog (x:int) (y:int) =
+let eval_program_old prog (x:int) (y:int) =
   let env = Hashtbl.of_alist_exn (module String) [("rax",x);("rbx",y)] in
   List.iter prog ~f:(function
     | Add (d,r1,r2) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env r1 + Hashtbl.find_exn env r2)
@@ -37,51 +37,57 @@ let eval_program prog (x:int) (y:int) =
   );
   env
 
+  let eval_program prog (init_env : (string, int) Hashtbl.t) =
+  let env = Hashtbl.copy init_env in
+  List.iter prog ~f:(function
+    | Add (d,r1,r2) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env r1 + Hashtbl.find_exn env r2)
+    | Mul (d,r1,r2) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env r1 * Hashtbl.find_exn env r2)
+    | Mov (d,s) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env s)
+    | IAdd (d,r,c) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env r + c)
+    | IMul (d,r,c) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env r * c)
+    | Load (d,c) -> Hashtbl.set env ~key:d ~data:c
+  );
+  env
 
-let run_program prog (x, y) = Hashtbl.find_exn (eval_program prog x y) "rax"
+let run_program_old prog (x, y) = Hashtbl.find_exn (eval_program_old prog x y) "rax"
+
+let run_program prog (init_env : (string,int) Hashtbl.t) ~out_reg = 
+  Hashtbl.find_exn (eval_program prog init_env) out_reg
 
 
-let check_equiv prog1 prog2 =
+let check_equiv prog1 prog2 regs =
   let ctx = mk_context [] in
   let int_sort = Arithmetic.Integer.mk_sort ctx in
-  let x = Expr.mk_const ctx (Symbol.mk_string ctx "rax") int_sort in
-  let y = Expr.mk_const ctx (Symbol.mk_string ctx "rbx") int_sort in
-  let init_env () =
-    Hashtbl.of_alist_exn (module String) [ ("rax", x); ("rbx", y) ]
-  in
+  let env1 = Hashtbl.of_alist_exn (module String)
+      (List.map regs ~f:(fun r -> r, Expr.mk_const ctx (Symbol.mk_string ctx r) int_sort)) in
+  let env2 = Hashtbl.copy env1 in
+
   let exec ctx prog env =
     List.iter prog ~f:(function
-      | Add (d, r1, r2) ->
-          let v = Arithmetic.mk_add ctx [ Hashtbl.find_exn env r1;
-                                          Hashtbl.find_exn env r2 ] in
-          Hashtbl.set env ~key:d ~data:v
-      | Mul (d, r1, r2) ->
-          let v = Arithmetic.mk_mul ctx [ Hashtbl.find_exn env r1;
-                                          Hashtbl.find_exn env r2 ] in
-          Hashtbl.set env ~key:d ~data:v
-      | Mov (d, s) ->
-          Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env s)
-      | IAdd (d,r,c) ->
-        let v = Arithmetic.mk_add ctx [Hashtbl.find_exn env r; Arithmetic.Integer.mk_numeral_i ctx c] in
-        Hashtbl.set env ~key:d ~data:v
-      | IMul (d, r, c) ->
-        let v = Arithmetic.mk_mul ctx [Hashtbl.find_exn env r; Arithmetic.Integer.mk_numeral_i ctx c] in 
-        Hashtbl.set env ~key:d  ~data:v
-      | Load (d, c) -> 
-        Hashtbl.set env ~key:d ~data:(Arithmetic.Integer.mk_numeral_i ctx c));
+      | Add (d,r1,r2) -> Hashtbl.set env ~key:d ~data:(Arithmetic.mk_add ctx [Hashtbl.find_exn env r1; Hashtbl.find_exn env r2])
+      | Mul (d,r1,r2) -> Hashtbl.set env ~key:d ~data:(Arithmetic.mk_mul ctx [Hashtbl.find_exn env r1; Hashtbl.find_exn env r2])
+      | Mov (d,s) -> Hashtbl.set env ~key:d ~data:(Hashtbl.find_exn env s)
+      | IAdd (d,r,c) -> Hashtbl.set env ~key:d ~data:(Arithmetic.mk_add ctx [Hashtbl.find_exn env r; Arithmetic.Integer.mk_numeral_i ctx c])
+      | IMul (d,r,c) -> Hashtbl.set env ~key:d ~data:(Arithmetic.mk_mul ctx [Hashtbl.find_exn env r; Arithmetic.Integer.mk_numeral_i ctx c])
+      | Load (d,c) -> Hashtbl.set env ~key:d ~data:(Arithmetic.Integer.mk_numeral_i ctx c)
+    );
     env
   in
-  let env1 = exec ctx prog1 (init_env ()) in
-  let env2 = exec ctx prog2 (init_env ()) in
-  let out1 = Hashtbl.find_exn env1 "rax" in
-  let out2 = Hashtbl.find_exn env2 "rax" in
-  let neq = Boolean.mk_not ctx (Boolean.mk_eq ctx out1 out2) in
+
+  let env1 = exec ctx prog1 env1 in
+  let env2 = exec ctx prog2 env2 in
+
+  let neq =
+    List.map regs ~f:(fun r -> Boolean.mk_not ctx (Boolean.mk_eq ctx (Hashtbl.find_exn env1 r) (Hashtbl.find_exn env2 r)))
+    |> Boolean.mk_or ctx
+  in
+
   let solver = Solver.mk_simple_solver ctx in
   Solver.add solver [neq];
   match Solver.check solver [] with
   | Solver.UNSATISFIABLE -> true
   | _ -> false
-;;
+
 
 let used_regs prog =
   let regs_of_instr = function
@@ -104,7 +110,7 @@ let random_instr_used_regs regs =
   | 2 -> IAdd (rand_choice regs, rand_choice regs, Random.int 10)
   | 3 -> Mov (rand_choice regs, rand_choice regs)
   | 4 -> IMul (rand_choice regs, rand_choice regs, Random.int 10)
-  | 5 -> Load ("rax", Random.int 10)
+  | 5 -> Load (rand_choice regs, Random.int 10)
   | _ -> failwith "unreachable"
 
 let perturb prog =
@@ -119,26 +125,11 @@ let perturb prog =
     | 1 ->
         prog @ [ random_instr_used_regs regs ]
     | 2 ->
-      
       if List.length prog > 1 then
         let i = Random.int (List.length prog) in
         List.filteri prog ~f:(fun j _ -> j <> i)
       else prog
     | _ -> prog
-;;
-
-
-let test_equiv_random prog1 prog2 trials =
-  let same = ref 0 in
-  for _ = 1 to trials do
-    let x = 0 in
-    let y = Random.int 5 in
-    let r1 = run_program prog1 (x, y) in
-    let r2 = run_program prog2 (x, y) in
-    if r1 = r2 then incr same
-  done;
-  !same
-;;
 
 let cost prog =
   Core.List.fold prog ~init:0.0 ~f:(fun acc inst ->
@@ -153,27 +144,42 @@ let cost prog =
 ;;
 
 
-let score_candidate prog_orig prog_candidate =
-  let random_matches = test_equiv_random prog_orig prog_candidate 10 in
+let test_equiv_random prog1 prog2 regs trials =
+  let same = ref 0 in
+  for _ = 1 to trials do
+    let env = Hashtbl.of_alist_exn (module String)
+      (List.map regs ~f:(fun r -> (r, Random.int 10)))
+    in
+    let out_reg = "rax" in
+    let r1 = run_program prog1 env ~out_reg in
+    let r2 = run_program prog2 env ~out_reg in
+    if r1 = r2 then incr same
+  done;
+  !same
+;;
+
+let score_candidate prog_orig prog_candidate regs =
+  let random_matches = test_equiv_random prog_orig prog_candidate regs 10 in
   let prop = float_of_int random_matches /. 10.0 in
   if Float.(prop <> 1.0) then
     0.1 +. prop
-  else if check_equiv prog_orig prog_candidate then
+  else if check_equiv prog_orig prog_candidate regs then
     2.0 +. 2.0 ** (-1.0 *. (cost prog_candidate))
   else 0.1 +. prop
 ;;
 
-let run_equiv_optimizer ~iterations prog0 =
+(*remove best_program and best_score here, we only need the list of visited programs.*)
+let run_equiv_optimizer ~iterations prog0 regs =
   Random.self_init ();
   printf "Initial program: %s\n" (string_of_program prog0);
 
   let current_prog = ref prog0 in
-  let current_score = ref (score_candidate prog0 prog0) in
+  let current_score = ref (score_candidate prog0 prog0 regs) in
   let visited = ref [(!current_prog, !current_score)] in
 
   for iter = 1 to iterations do
     let candidate = perturb !current_prog in
-    let score = score_candidate prog0 candidate in
+    let score = score_candidate prog0 candidate regs in
 
     let accept_ratio = score /. !current_score in
     let r = Random.float 1.0 in
@@ -196,26 +202,21 @@ let run_equiv_optimizer ~iterations prog0 =
   (!current_prog, !current_score, visited_sorted)
 ;;
 
-
-(* constant folding: [Load("rax", 1); IAdd("rax", 1); IMul("rax", 2)] gets optimized to Load("rax", 4)*)
-
 let run () =
-  let prog0 = [IMul("rax", "rax", 2)] in
+  let prog0 = [Load("rax", 1); IAdd("rax", "rax", 1); Load("rbx", 2); Mul("rax", "rax", "rbx")] in
+  let regs = ["rax"; "rbx"] in
 
-  let x = run_program prog0 (0, 0) in
+  let x = run_program prog0 (Hashtbl.of_alist_exn (module String) [("rax",0); ("rbx",0)]) ~out_reg:"rax" in
   printf "Result of running %s \n" (string_of_program prog0);
   printf "Is === %d === \n" x;
 
-  let best_prog, best_score, visited = run_equiv_optimizer ~iterations:1000 prog0 in
-
-  printf "\nFinal best program: %s\n" (string_of_program best_prog);
-  printf "Score: %.2f\n" best_score;
+  let _,_, visited = run_equiv_optimizer ~iterations:10000 prog0 regs in
 
   printf "\n Original program: %s\n" (string_of_program prog0);
 
   printf "\nTop 5 visited programs:\n";
   List.iter (List.take visited 5) ~f:(fun (prog, score) ->
-    printf "Score %.5f -- result: %d --- %s\n" score ((run_program prog) (0,0)) (string_of_program prog) 
+    let env = Hashtbl.of_alist_exn (module String) [("rax",0); ("rbx",0)] in
+    printf "Score %.5f -- result: %d --- %s\n" score (run_program prog env ~out_reg:"rax") (string_of_program prog) 
   )
 ;;
-
